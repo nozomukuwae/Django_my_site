d91456cfd754b3b87894b74b75941621874a8175
Nozomu Kuwae <nozomukuwae@gmail.com>
2023-04-22 18:20:51 +0900

    Link pages

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.starting_page, name='starting_page'),
+    path('', views.starting_page, name='starting-page'),
     path('posts/', views.posts, name='posts-page'),
     path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),
 ]
